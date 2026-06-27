f86b21982276b3589aa7ff3549f1db96841e3bea
mabdurehman506-dev <mabdurehman506@gmail.com>
2026-06-27 13:17:03 +0500

    course

--- promise.js
+++ promise.js
@@ -1,11 +1,27 @@
-function getData(dataId, callback) {
+const getPromise = () => {
   return new Promise((resolve, reject) => {
-    setTimeout(function () {
-      console.log("Data fetched for ID:", dataId);
-      resolve("Success");
-      if (callback) {
-        callback();
-      }
-    }, 15000);
-  });
-}
+    console.log("Promise started");
+//    resolve("Promise resolved");
+    reject("Promise rejected");
+    });
+};
+
+const promise = getPromise();
+promise.then(() => {
+  console.log("promise fullfilled");
+});
+promise.catch((error) => {
+  console.log("promise rejected with error:", error);
+});
+
+// function getData(dataId, callback) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(function () {
+//       console.log("Data fetched for ID:", dataId);
+//       resolve("Success");
+//       if (callback) {
+//         callback();
+//       }
+//     }, 15000);
+//   });
+// }
